74050cb49b5debe54b8c527165b624c5b9da38b1
lara <larakhtb99>
2022-01-20 18:28:11 +0100

    Docker test unitaire

---
+++ src/systeminformation.spec.ts
@@ -0,0 +1,8 @@
+import { si } from './systeminformation';
+
+describe('typeScript test suite', () => {
+  it('should return "Cpu if exists or not"', () => {
+    expect.assertions(1);
+    expect(si).toHaveProperty('cpu');
+  });
+});
