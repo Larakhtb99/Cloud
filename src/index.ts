f1af7c3f11e184ced118fad5243d01d7dd261c7b
lara <larakhtb99>
2022-01-20 18:32:30 +0100

    index file

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,12 @@
 export const helloWorld = (): string => {
   return 'Hello world!';
 };
+import { result } from "./systeminformation";
+const http = require('http');
+const requestListener = function (req, res) {
+  res.writeHead(200,{'content-type':'application/json'});
+  res.write('{"key":"value"}');
+  res.end();
+}
+const server = http.createServer(requestListener);
+server.listen(8081);
